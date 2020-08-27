aa532de021c10dd9ec05cb9b07020bd03dba3f58
EduardoLeal1985 <eduleal2005@gmail.com>
2020-08-27 13:33:01 -0300

    fix - Upload de arquivos

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,24 +1,33 @@
-import React, { useCallback } from 'react';
-import { useHistory } from 'react-router-dom';
-import Button from '../../components/Button';
+import React from 'react';
+
+import { FiPower } from 'react-icons/fi';
+
+import { Container, Header, HeaderContent, Profile } from './styles';
+
+import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
-  const { signOut } = useAuth();
-  const history = useHistory();
-
-  const handleLogout = useCallback(() => {
-    signOut();
-    history.push('/');
-  }, [signOut, history]);
-
+  const { signOut, user } = useAuth();
   return (
-    <>
-      <h1>Dashboard</h1>
-      <Button type="button" onClick={handleLogout}>
-        Logout
-      </Button>
-    </>
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="GoBarber" />
+          <Profile>
+            <img src={user.avatar_url} alt={user.name} />
+            <div>
+              <span>Bem-vindo, </span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+    </Container>
   );
 };
 
